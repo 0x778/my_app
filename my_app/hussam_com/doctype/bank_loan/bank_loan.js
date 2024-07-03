77ee586e86821cd29c0eecc7509c96cd2e23f5b7
0x778 <hussamalmaswari@gmail.com>
2024-07-03 06:36:07 +0300

    function for field repayment months in bank loan

--- my_app/hussam_com/doctype/bank_loan/bank_loan.js
+++ my_app/hussam_com/doctype/bank_loan/bank_loan.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2024, Hussam and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Bank Loan", {
+ frappe.ui.form.on("Bank Loan", {
 // 	refresh(frm) {
 
 // 	},
-// });
+
+repayment_months : function(frm){
+    if(frm.doc.loan_amount && frm.doc.repayment_months){
+       let amount = frm.doc.loan_amount / frm.doc.repayment_months ;
+       frm.doc.monthly_repayment = amount ;
+    }
+
+}
+
+
+
+
+});
