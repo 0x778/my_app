c0fa5fa9f7846f9e000925fbafe3edf560cf83a9
0x778 <hussamalmaswari@gmail.com>
2024-07-03 08:00:34 +0300

    Done js in bank loan doctype

--- my_app/hussam_com/doctype/bank_loan/bank_loan.js
+++ my_app/hussam_com/doctype/bank_loan/bank_loan.js
@@ -7,14 +7,30 @@
 // 	},
 
 repayment_months : function(frm){
-    if(frm.doc.loan_amount && frm.doc.repayment_months){
-       let amount = frm.doc.loan_amount / frm.doc.repayment_months ;
-       frm.doc.monthly_repayment = amount ;
-    }
-
+calculate(frm);
+},
+interest : function(frm){
+calculate(frm);
+},
+loan_amount : function(frm){
+    calculate(frm);
 }
 
 
 
 
 });
+
+function calculate(frm){
+    if(frm.doc.loan_amount && frm.doc.repayment_months){
+        let amount = frm.doc.loan_amount / frm.doc.repayment_months ;
+       // amount = amount + amount *  frm.doc.interest / 100 ;
+        frm.doc.monthly_repayment = amount ;
+        //console.log(frm.doc.monthly_repayment);
+        refresh_field("monthly_repayment");
+     }
+     else{
+        frm.doc.monthly_repayment = 0 ;
+        refresh_field("monthly_repayment");
+     }
+}
